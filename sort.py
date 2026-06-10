ba64c7283985fd2cefcfd8a327b17badd44b13fc
이희승 <heseung0895@naver.com>
2026-06-11 01:40:23 +0900

    거의 끝

--- sort.py
+++ sort.py
@@ -33,26 +33,35 @@ def BUBBLE_DESC(arr):
     return f"버블 내림차순: {arr}, 정렬 횟수: {a}"
 
 def SELECT_ASC(arr):
-    a=0
+    a = 0
     for i in range(len(arr)-1):
+        min_idx = i 
+
         for j in range(i+1, len(arr)):
-            if arr[i] > arr[j]:
-                tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
-                a+=1
+            if arr[min_idx] > arr[j]:
+                min_idx = j  
+        
+        if min_idx != i:
+            tmp = arr[i]
+            arr[i] = arr[min_idx]
+            arr[min_idx] = tmp
+            a += 1 
             
     return f"선택 오름차순: {arr}, 정렬 횟수: {a}"
 
 def SELECT_DESC(arr):
     a=0
     for i in range(len(arr)-1):
+        max_idx = i
         for j in range(i+1, len(arr)):
-            if arr[i] < arr[j]:
-                tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
-                a+=1
+            if arr[max_idx] < arr[j]:
+                max_idx = j
+        
+        if max_idx != i:
+            tmp = arr[i]
+            arr[i] = arr[max_idx]
+            arr[max_idx] = tmp
+            a+=1
             
     return f"선택 내림차순: {arr}, 정렬 횟수: {a}"
 
