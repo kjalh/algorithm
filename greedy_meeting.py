56aa54bd80bc0050442af42a924ed2fe6d9a6469
이희승 <heseung0895@naver.com>
2026-06-22 20:03:38 +0900

    회의 시간 배분 완성

--- greedy_meeting.py
+++ greedy_meeting.py
@@ -1,26 +1,24 @@
-for i in range(3):
-    print("3번 반복")
+print("3번 반복")
+m = []
+
+for i in range(3):  
     t = input("팀 명: ")
     sm = int(input("회의 시작 시간: "))
     em = int(input("회의 종료 시간: "))
 
-    m = []
-
-    if sm > 12:
-        sm -= 12
     
-    if em > 12:
-        em -= 12
-    
-    m.append(t, sm, em)
+    m.append([t, sm, em])
+
+m.sort(key=lambda x: (x[2], x[1]))
 
 
-m.sort(x= lambda x: x[2])
-    
-
+for i in range(2): 
+    tmp = m[i+1][2]-m[i+1][1]   # 순수 회의 시간
+    m[i+1][1] = m[i][2]         # 앞에 끝난 시간이 다음 시작 시간
+    m[i+1][2] = m[i+1][1] + tmp # 시간 배분
 
 for i in range(3):
-    print(f"{i+1}번: {m[i][0]}")
+    print(f"{i+1}번: {m[i][0]}  시간 {m[i][1]} ~ {m[i][2]}")
 
 
     
